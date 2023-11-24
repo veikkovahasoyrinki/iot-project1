ab0f429a6b8159933dc3f0eca0f7e5e3b9e2352f
Veikko Vähäsöyrinki <veikko.vahasoyrinki@protonmail.ch>
2023-11-24 09:30:16 +0200

    Remove unused files and updated main.c, added server.py

--- Makefile
+++ Makefile
@@ -2,13 +2,26 @@ APPLICATION = IOT2023Project1
 
 BOARD ?= iotlab-m3
 
+USEMODULE += ipv6_addr
 USEMODULE += shell
 USEMODULE += shell_commands
 USEMODULE += ztimer_msec
 
+USEMODULE += gcoap
+USEMODULE += auto_init_gnrc_netif
+  # Specify the mandatory networking modules
+USEMODULE += gnrc_ipv6_default
+USEMODULE += od
+USEMODULE += fmt
+USEMODULE += netutils
+USEMODULE += random
+# Add also the shell, some shell commands
+USEMODULE += shell
+USEMODULE += shell_cmds_default
+USEMODULE += ps
 USEMODULE += lsm303dlhc
 
-RIOTBASE ?= $(CURDIR)/../RIOT-master
+RIOTBASE ?= $(CURDIR)/../RIOT
 
 include $(RIOTBASE)/Makefile.include
 
--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 #include "shell.h"
 
 #include "mutex.h"
+#include <arpa/inet.h>
 
+#include "fmt.h"
+#include "net/gcoap.h"
+#include "net/sock/util.h"
+#include "net/utils.h"
 /* Add lsm303dlhc related include here */
 #include "lsm303dlhc.h"
 #include "lsm303dlhc_params.h"
@@ -21,10 +26,48 @@ static mutex_t lsm_lock = MUTEX_INIT_LOCKED;
 /* stack memory allocated for the lsm303dlhc thread */
 static char lsm303dlhc_stack[THREAD_STACKSIZE_MAIN];
 
+void send_accelerometer_data(lsm303dlhc_3d_data_t acc_value) {
+    char address[IPV6_ADDR_MAX_STR_LEN] = "2600:1900:4150:a91b::";
+    uint16_t port = 8683;
+    ipv6_addr_t addr; 
+    uint8_t payload[128]; // Adjust payload buffer size as needed
+    uint8_t buf[CONFIG_GCOAP_PDU_BUF_SIZE];
+    sock_udp_ep_t remote = SOCK_IPV6_EP_ANY;
+    coap_pkt_t pdu;
+    size_t payload_len = snprintf((char *)payload, sizeof(payload),
+                                  "x: %i y: %i z: %i\n",
+                                  acc_value.x_axis, acc_value.y_axis, acc_value.z_axis);
+
+    gcoap_req_init(&pdu, &buf[0], CONFIG_GCOAP_PDU_BUF_SIZE, 1, "/whoami");
+    // Copy payload into CoAP packet
+    coap_hdr_set_type(pdu.hdr, 1);
+    memcpy(pdu.payload, payload, payload_len);
+    pdu.payload_len = payload_len;
+
+    // Finish the CoAP request
+    coap_opt_finish(&pdu, COAP_OPT_FINISH_NONE);
+
+    remote.family = AF_INET6;
+    remote.port   = port;
+    //remote.addr.ipv6   = addr;
+    ipv6_addr_from_str(&addr, address);
+    memcpy(&remote.addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
+    //sock_udp_name2ep(&remote, address);
+    //new_remote = &remote;
+    // Send CoAP request        
+    printf("gcoap_cli: sending msg ID %u, %u bytes\n", coap_get_id(&pdu),
+               (unsigned) payload_len);
+    printf("Port: %x",*remote.addr.ipv6);
+    gcoap_req_send(&buf[0],payload_len,&remote,NULL, NULL);
+
+    printf("CoAP POST request sent successfully!\n");
+
+}
+
 static void *lsm303dlhc_thread(void *arg)
 {
     (void)arg;
-
+    //char addr[24]     = "[2600:1900:4150:a91b::]";
     while (1) {
         /* Acquire the mutex here */
         mutex_lock(&lsm_lock);
@@ -42,11 +85,18 @@ static void *lsm303dlhc_thread(void *arg)
         /* Release the mutex here */
         mutex_unlock(&lsm_lock);
 
-        ztimer_sleep(ZTIMER_MSEC, 500);
+
+        send_accelerometer_data(acc_value);
+
+
+        ztimer_sleep(ZTIMER_MSEC, 1000);
     }
 
     return 0;
 }
+
+
+
 
 static void _lsm303dlhc_usage(char *cmd)
 {
